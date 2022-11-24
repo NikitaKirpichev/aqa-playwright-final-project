b97c3a69b111692110ed4fabbf829759455292b5
Nikita Kirpichoev <nalkire17@gmail.com>
2022-11-24 08:07:21 +0300

    onliner project init

---
+++ src/onliner/tests/onliner.spec.ts
@@ -0,0 +1,14 @@
+import { test } from "@playwright/test";
+import { HomePage } from "../pages/home.page";
+
+const productName = 'Смартфон Samsung Galaxy A52 SM-A525F/DS 4GB/128GB (черный)';
+
+test.describe("Search, add and delete from cart product", () =>{
+    test("Search products", async({page}) => {
+        const search = new HomePage(page);
+
+        await page.goto("https://www.onliner.by/");
+
+        await search.enterProductName(productName);
+    })
+})
